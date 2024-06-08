abb8d4f210416050efca9907ef54c947363ed8d3
Aluno Manhã <michaeloliveira38@gmail.com>
2024-06-07 21:19:39 -0300

    VErsão 2.0

--- cad_usuario.py
+++ cad_usuario.py
@@ -15,6 +15,11 @@ Ao entrar no aplicativo, o usuário pode:
 #Importe das Bibliotecas:
 import os
 import time
+from menu import *
+from cad_usuario import *
+from mostrar_dados import *
+from atualizandor_dados import *
+from mostrar_dados import *
 
 
 #funções do programa:
@@ -23,29 +28,6 @@ import time
 chaves = ('Nome', 'CPF', 'Telefone', 'E-mail', 'Profissão', 'Empresa','Conta-Cheque')
 # Lista para armazenar os dados das pessoas
 dados = []
-
-
-def menu():
-    while True:
-        print(f"\n{'='*20} Bem Vindo ao Mamute Bank: {'='*20}")
-        print(f"{'='*20} ESCOLHA UMA DAS OPÇÕES PARA INICIAR: {'='*20}")
-        print("1. Quero ser cliente do banco")
-        print("2. Deletar dados do usuario")
-        print("3. entrar no aplicativo")
-        print("4. Sair")
-        opcao = input("Escolha uma opção: ")
-        os.system('cls')  # Limpa a tela
-
-        if opcao == '1':
-            inserir()
-        elif opcao == '2':
-            indice = int(input("Informe o índice da pessoa que deseja excluir: "))
-            excluir_dados(indice)
-        elif opcao == '3':
-            print("Saindo do programa...")
-            break
-        else:
-            print("Opção inválida. Tente novamente.")
 
 
 def inserir():
@@ -57,20 +39,17 @@ def inserir():
         chaves[3]: input('Informe o seu e-mail: '),
         chaves[4]: input('Informe sua profissão: '),
         chaves[5]: input('Informe o nome da Empresa que você trabalha: '),
-        chaves[6]: (input('Informe o valor do seu primeiro deposito em R$').replace(',','.'))
+        chaves[6]: (input('Informe o valor do seu primeiro deposito em R$ ').replace(',','.'))
     }
     
-    print('Inserindo Dados...')
-    time.sleep(2)  # Espera por 2 segundos para simular a inserção
-    dados.append(pessoa)  # Adiciona a nova pessoa à lista 'dados'
-    print('Cadastro realizado com sucesso!')
-    time.sleep(2)  # Espera por 2 segundos para que o usuário veja a mensagem
+    
 
 
     while True:
         opcao = input(
             "Deseja voltar ao menu principal (s) ou sair do programa (n)? ").lower()
         if opcao == 's':
+            menu ()
             break
         elif opcao == 'n':
             print("Saindo do programa...")
@@ -79,16 +58,3 @@ def inserir():
             exit()
         else:
             print("Opção inválida. Por favor, escolha 's' para voltar ao menu principal ou 'n' para sair do programa.")
-
-
-def excluir_dados(indice):
-    if indice < 1 or indice > len(dados):
-        print("Índice inválido.")
-        return
-
-    dados.pop(indice - 1)  # Remove a pessoa pelo índice
-    print(f"Item de índice {indice} removido com sucesso.")
-    time.sleep(2)  # Espera por 2 segundos para que o usuário veja a mensagem
-    os.system('cls')  # Limpa a tela
-
-
