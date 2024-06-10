409e5e8c7a129c1c2bb76111bc07d9ff14aad7c4
MichaelOli <michaeloliveira38@gmail.com>
2024-06-10 19:13:02 -0300

    Versão-1.6

--- cad_usuario.py
+++ cad_usuario.py
@@ -1,149 +1,57 @@
-'''Crie um aplicativo de banco, onde o usuário possa:
+from datetime import datetime
 
-- Cadastrar seu nome no aplicativo.
-- Deletar seu nome do aplicativo.
-- Entrar no aplicativo.
+# Obtendo a data atual
+data_atual = datetime.now()
 
-Ao entrar no aplicativo, o usuário pode:
+# Formatando a data para o padrão brasileiro
+data_formatada = data_atual.strftime("%d/%m/%Y")
 
-- Consultar seu saldo(criar uma nova conta o saldo começa com R$ 0, 00
-                      a consulta deverá exibir a data da consulta)
-- Depositar valor.
-- Sacar valor.
-- Sair do programa.'''
-
-#Importe das Bibliotecas:
-import os
-import time
-from cad_usuario import *
+usuarios = {}
 
 
-# IDs das entradas
-chaves = ('Nome', 'CPF', 'Telefone', 'E-mail',
-          'Profissão', 'Empresa', 'Conta-Cheque')
-# Lista para armazenar os dados das pessoas
-dados = []
-
-
-def menu():
-    while True:
-        print(f"\n{'='*20} Bem Vindo ao Mamute Bank: {'='*20}")
-        print(f"{'='*20} ESCOLHA UMA DAS OPÇÕES PARA INICIAR: {'='*20}")
-        print("1. Quero ser cliente do banco")
-        print("2. Deletar dados da conta")
-        print("3. Dados da conta.")
-        print("4. Atualizar Saldo da conta.")
-        print("4. Sair")
-        opcao = input("Escolha uma opção: ")
-        os.system('cls')  # Limpa a tela
-
-        if opcao == '1':
-            inserir ()
-            print('Inserindo Dados...')
-            time.sleep(2)  # Espera por 2 segundos para simular a inserção
-            dados.append(inserir.pessoa)  # Adiciona a nova pessoa à lista 'dados'
-            print('Cadastro realizado com sucesso!')
-            # Espera por 2 segundos para que o usuário veja a mensagem
-            time.sleep(2)
-            time.sleep(2)
-            os.system('cls')
-        elif opcao == '2':
-            indice = int(
-                input("Informe o índice da pessoa que deseja excluir: "))
-            excluir_dados(indice)
-        elif opcao == '3':
-            listar_dados()
-            while True:
-                opcao = input(
-                    "Deseja voltar ao menu principal (s) ou sair do programa (n)? ").lower()
-                if opcao == 's':
-                    menu.menu()
-                    break
-                elif opcao == 'n':
-                    print("Saindo do programa...")
-                    # Espera por 2 segundos para simular saida do programa.
-                    time.sleep(2)
-                    exit()
-                else:
-                    print(
-                        "Opção inválida. Por favor, escolha 's' para voltar ao menu principal ou 'n' para sair do programa.")
-
-            # Pergunta ao usuário se deseja voltar ao menu principal ou sair do programa
-            break
-        elif opcao == '4':
-            listar_dados()
-            atualizar_dados()
-            indice = int(
-                input("Informe o índice da pessoa que deseja atualizar: "))
-            atualizar_dados(indice)
-        else:
-            print("Opção inválida. Tente novamente.")
-
-
-def inserir():
-    # Coleta os dados da nova pessoa
-    pessoa = {
-        chaves[0]: input('Informe seu nome: '),
-        chaves[1]: input('Informe o seu CPF: '),
-        chaves[2]: input('Informe o  seu telefone: '),
-        chaves[3]: input('Informe o seu e-mail: '),
-        chaves[4]: input('Informe sua profissão: '),
-        chaves[5]: input('Informe o nome da Empresa que você trabalha: '),
-        chaves[6]: (
-            input('Informe o valor do seu primeiro deposito em R$ ').replace(',', '.'))
+def cadastrar_usuario(nome, cpf, telefone, email, profissao, empresa, deposito_inicial):
+    deposito_inicial = deposito_inicial.replace(',', '')
+    saldo_inicial = float(deposito_inicial)
+    if cpf in usuarios:
+        return "Usuário já cadastrado."
+    usuarios[cpf] = {
+        'nome': nome,
+        'telefone': telefone,
+        'email': email,
+        'profissao': profissao,
+        'empresa': empresa,
+        'saldo': saldo_inicial,
+        'data_criacao': data_formatada
     }
-
-    while True:
-        opcao = input(
-            "Deseja voltar ao menu principal (s) ou sair do programa (n)? ").lower()
-        if opcao == 's':
-            menu()
-            break
-        elif opcao == 'n':
-            print("Saindo do programa...")
-            # Espera por 2 segundos para simular saida do programa.
-            time.sleep(2)
-            exit()
-        else:
-            print("Opção inválida. Por favor, escolha 's' para voltar ao menu principal ou 'n' para sair do programa.")
+    return "Usuário cadastrado com sucesso."
 
 
-def listar_dados():
-    if not dados:
-        # Informa se não há dados cadastrados
-        print("Nenhum dado cadastrado...")
-    else:
-        print('Pesquisando Dados...')  # Mensagem de espera e tempo de espera
-        time.sleep(2)  # Espera por 2 segundos para simular a pesquisa.
-        for i, pessoa in enumerate(dados):
-            print(f"{i+1}.")  # Exibe o número da pessoa na lista
-            for chave, valor in pessoa.items():
-                # Exibe cada par chave-valor da pessoa
-                print(f"{chave}: {valor}")
-
-        print('Dados listados com sucesso!')
+def deletar_usuario(cpf):
+    if cpf in usuarios:
+        del usuarios[cpf]
+        return "Usuário deletado com sucesso."
+    return "Usuário não encontrado."
 
 
-def atualizar_dados(indice):
-    if indice < 1 or indice > len(dados):
-        print("Índice inválido.")
-        return
-
-    pessoa = dados[indice - 1]  # Acessa a pessoa pelo índice
-    for chave in chaves:
-        # Pergunta o novo valor de cada chave
-        novo_valor = input(f"{chave[6]} atual ({pessoa[chave[6]]}): ")
-        if novo_valor:
-            # Atualiza o valor se o usuário forneceu uma nova entrada
-            pessoa[chave[6]] = novo_valor
+def consultar_saldo(cpf):
+    if cpf in usuarios:
+        saldo = usuarios[cpf]['saldo']
+        data_consulta = data_formatada
+        return f"Saldo: R$ {saldo:.2f}\nData da consulta: {data_consulta}"
+    return "Usuário não encontrado."
 
 
-def excluir_dados(indice):
-    if indice < 1 or indice > len(dados):
-        print("Índice inválido.")
-        return
+def depositar_valor(cpf, valor):
+    if cpf in usuarios:
+        usuarios[cpf]['saldo'] += valor
+        return f"Depósito de R$ {valor:.2f} realizado com sucesso."
+    return "Usuário não encontrado."
 
-    dados.pop(indice - 1)  # Remove a pessoa pelo índice
-    print(f"Item de índice {indice} removido com sucesso.")
-    time.sleep(2)  # Espera por 2 segundos para que o usuário veja a mensagem
-    os.system('cls')  # Limpa a tela
+
+def sacar_valor(cpf, valor):
+    if cpf in usuarios:
+        if usuarios[cpf]['saldo'] >= valor:
+            usuarios[cpf]['saldo'] -= valor
+            return f"Saque de R$ {valor:.2f} realizado com sucesso."
+        return "Saldo insuficiente."
+    return "Usuário não encontrado."
--- main.py
+++ main.py
@@ -1,3 +1,78 @@
+import os
+import time
 from cad_usuario import *
+from datetime import datetime
 
-menu()
+data_atual = datetime.now()
+data_formatada = data_atual.strftime("%d/%m/%Y")
+
+
+def menu_principal():
+    while True:
+        print(f"{"="*20} Bem Vindo ao Mamute Bank {"="*20}")
+        print("1. Quero ser cliente do banco")
+        print("2. Deletar dados da conta")
+        print("3. Dados da conta")
+        print("4. Atualizar Saldo da conta")
+        print("5. Sair")
+        opcao = input("Escolha uma opção: ")
+        os.system('cls')
+
+        if opcao == '1':
+            inserir()
+        elif opcao == '2':
+            cpf = input("Informe o CPF da conta a ser deletada: ")
+            print(deletar_usuario(cpf))
+        elif opcao == '3':
+            cpf = input("Informe o CPF da conta: ")
+            print(consultar_saldo(cpf))
+        elif opcao == '4':
+            atualizar_saldo()
+        elif opcao == '5':
+            print("Saindo do programa...")
+            time.sleep(2)
+            break
+        else:
+            print("Opção inválida. Tente novamente.")
+
+
+def inserir():
+    nome = input('Informe seu nome: ')
+    cpf = input('Informe o seu CPF: ')
+    telefone = input('Informe o seu telefone: ')
+    email = input('Informe o seu e-mail: ')
+    profissao = input('Informe sua profissão: ')
+    empresa = input('Informe o nome da Empresa que você trabalha: ')
+    deposito_inicial = input(
+        'Informe o valor do seu primeiro deposito em R$ ').replace(',', '.')
+
+    print(cadastrar_usuario(nome, cpf, telefone,
+          email, profissao, empresa, deposito_inicial))
+
+
+def atualizar_saldo():
+    cpf = input("Informe o CPF da conta: ")
+    if cpf not in usuarios:
+        print("Usuário não encontrado.")
+        return
+
+    while True:
+        print("\n1. Depositar Valor")
+        print("2. Sacar Valor")
+        print("3. Voltar ao Menu Principal")
+        opcao = input("Escolha uma opção: ")
+
+        if opcao == '1':
+            valor = float(input("Digite o valor para depósito: R$ "))
+            print(depositar_valor(cpf, valor))
+        elif opcao == '2':
+            valor = float(input("Digite o valor para saque: R$ "))
+            print(sacar_valor(cpf, valor))
+        elif opcao == '3':
+            break
+        else:
+            print("Opção inválida. Tente novamente.")
+
+
+if __name__ == "__main__":
+    menu_principal()
